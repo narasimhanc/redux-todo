88f6dac4748a54e30ff3d81ee76777d13055af03
Andrew Van Slaars <avanslaars@gmail.com>
2017-05-14 14:24:00 -0400

    Add redux and create store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { createStore } from 'redux'
 
-const state = {
+const initState = {
   todos: [
     {id: 1, name:'Create Static UI', isComplete: true},
     {id: 2, name:'Create Initial State', isComplete: true},
@@ -12,7 +13,16 @@ const state = {
   ]
 }
 
+const reducer = function(state, action) {
+  console.log('state', state)
+  console.log('action', action)
+  return state
+}
+
+const store = createStore(reducer, initState)
+
+const state = store.getState()
 ReactDOM.render(
   <App todos={state.todos} />,
   document.getElementById('root')
-);
+)
