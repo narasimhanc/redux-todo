fab7686a8d9d24649d6b30689ea79c2117e381e9
Andrew Van Slaars <avanslaars@gmail.com>
2017-05-14 09:45:23 -0400

    Added state and passed in as props

--- src/App.js
+++ src/App.js
@@ -16,15 +16,7 @@ class App extends Component {
           </form>
           <div className="Todo-List">
             <ul>
-              <li>
-                <input type="checkbox" /> Create Static UI
-              </li>
-              <li>
-                <input type="checkbox" /> Create Initial State
-              </li>
-              <li>
-                <input type="checkbox" /> Use state to render UI
-              </li>
+              {this.props.todos.map(todo => <li key={todo.id}><input type="checkbox" defaultChecked={todo.isComplete} />{todo.name}</li>)}
             </ul>
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+const state = {
+  todos: [
+    {id: 1, name:'Create Static UI', isComplete: true},
+    {id: 2, name:'Create Initial State', isComplete: true},
+    {id: 3, name:'Use state to render UI', isComplete: false},
+    {id: 4, name:'Control state with Redux', isComplete: false}
+  ]
+}
+
 ReactDOM.render(
-  <App />,
+  <App todos={state.todos} />,
   document.getElementById('root')
 );
